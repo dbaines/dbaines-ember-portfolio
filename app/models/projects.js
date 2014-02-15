4546b9c9f36b19f981e98b06c3c8d45934455964
David Baines <dbaines@gmail.com>
2014-02-15 15:41:27 +1030

    Re-arranged projects and added tags

--- app/models/projects.js
+++ app/models/projects.js
@@ -2,136 +2,125 @@
 var Projects = DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  category: DS.attr('category'),
+  tags: DS.attr('string'),
   image_src: DS.attr('string'),
   images: DS.attr('array'),
 
   imagePath: function(){
-    return "/assets/" + this.get("category")  + "/" + this.get("image_src");
-  }.property('image_src', 'category'),
-  isArtwork: function(){
-    return this.get('category') === 'artwork';
-  }.property('category'),
-  isWebsite: function(){
-    return this.get('category') === 'website';
-  }.property('category')
+    var image_src = this.get("image_src");
+    if(image_src == null) {
+      return this.get("images")[0];
+    } else {
+      return "/assets/projects/" + image_src;
+    }
+  }.property('image_src', 'images')
 });
 
 Projects.FIXTURES = [
   {
-    id: "404-monkey",
-    title: "404 Monkey",
+    id: "artwork",
+    title: "Artwork",
     description: null,
-    category: "artwork",
-    image_src: "404monkey.jpg",
+    image_src: null,
+    images: [
+      "/assets/projects/artwork/404monkey.jpg",
+      "/assets/projects/artwork/afromunkee.jpg",
+      "/assets/projects/artwork/blade.jpg",
+      "/assets/projects/artwork/bloodyknifeguyredux_final.jpg",
+      "/assets/projects/artwork/gunmouthguy.jpg",
+      "/assets/projects/artwork/hairyguy.jpg",
+      "/assets/projects/artwork/jedirabbit.jpg",
+      "/assets/projects/artwork/knightguy.jpg",
+      "/assets/projects/artwork/leap.jpg",
+      "/assets/projects/artwork/lildemon.jpg",
+      "/assets/projects/artwork/ninja.jpg",
+      "/assets/projects/artwork/nuclear.jpg",
+      "/assets/projects/artwork/orc.jpg",
+      "/assets/projects/artwork/pirate.jpg",
+      "/assets/projects/artwork/shade-cartoony.jpg",
+      "/assets/projects/artwork/sithmonkey.jpg",
+      "/assets/projects/artwork/unomomento.jpg",
+      "/assets/projects/artwork/warturtle.jpg",
+      "/assets/projects/artwork/x5hoodie.jpg",
+      "/assets/projects/artwork/x5stabsguy.jpg"
+    ]
+  },
+  {
+    id: "pasta-mama",
+    title: "Pasta Mama",
+    tags: "ios app",
+    description: "..",
+    image_src: "pastamama.jpg",
+    images: [
+      // "/assets/projects/bellatextiles_002.jpg"
+    ]
+  },
+  {
+    id: "nikl-tin",
+    title: "Nikl-Tin",
+    tags: "ios app",
+    description: null,
+    image_src: "nikltin.jpg",
     images: null
   },
   {
-    id: "afromonkey",
-    title: "Afromonkey",
+    id: "garys-best-price",
+    title: "Gary's Best Price",
+    tags: "website",
     description: null,
-    category: "artwork",
-    image_src: "afromunkee.jpg",
+    image_src: "garysbestprice.jpg",
     images: null
   },
   {
-    id: "blade",
-    title: "Blade",
+    id: "christines-massage",
+    title: "Christine's Massage",
+    tags: "website",
     description: null,
-    category: "artwork",
-    image_src: "blade.jpg",
+    image_src: "christinesmassage.jpg",
     images: null
   },
   {
-    id: "knife-guy",
-    title: "Bloody Knife Guy",
+    id: "steel-blue-zebra",
+    title: "Steel Blue Zebra",
+    tags: "website",
     description: null,
-    category: "artwork",
-    image_src: "bloodyknifeguyredux_final.jpg",
+    image_src: "steel-blue-zebra.jpg",
     images: null
   },
   {
-    id: "gun-mouth",
-    title: "Gun Mouth Guy",
+    id: "demon-soul",
+    title: "Demon Soul",
+    tags: "motion",
     description: null,
-    category: "artwork",
-    image_src: "bloodyknifeguyredux_final.jpg",
+    image_src: "demon-soul.jpg",
     images: null
   },
   {
-    id: "hairy-guy",
-    title: "Hairy",
+    id: "city-race",
+    title: "City Race",
+    tags: "motion",
     description: null,
-    category: "artwork",
-    image_src: "hairyguy.jpg",
-    images: null
-  },
-  {
-    id: "jedi-rabbit",
-    title: "Jedi Rabbit",
-    description: null,
-    category: "artwork",
-    image_src: "jedirabbit.jpg",
-    images: null
-  },
-  {
-    id: "leap",
-    title: "Leap",
-    description: null,
-    category: "artwork",
-    image_src: "leap.jpg",
-    images: null
-  },
-  {
-    id: "lil-demon",
-    title: "Lil' Demon",
-    description: null,
-    category: "artwork",
-    image_src: "lildemon.jpg",
-    images: null
-  },
-  {
-    id: "ninja",
-    title: "Ninja",
-    description: null,
-    category: "artwork",
-    image_src: "ninja.jpg",
-    images: null
-  },
-  {
-    id: "nuclear",
-    title: "Nuclear",
-    description: null,
-    category: "artwork",
-    image_src: "nuclear.jpg",
-    images: null
-  },
-  {
-    id: "orc",
-    title: "Orc",
-    description: null,
-    category: "artwork",
-    image_src: "orc.jpg",
-    images: null
-  },
-  {
-    id: "pirate",
-    title: "Pirate",
-    description: null,
-    category: "artwork",
-    image_src: "pirate.jpg",
+    image_src: "city-race.jpg",
     images: null
   },
   {
     id: "bella-textiles",
     title: "Bella Textiles",
-    description: "..",
-    category: "website",
+    tags: "website",
+    description: null,
     image_src: "bellatextiles_001.jpg",
     images: [
-      "/assets/website/bellatextiles_002.jpg",
-      "/assets/website/bellatextiles_003.jpg"
+      "/assets/projects/bellatextiles_002.jpg",
+      "/assets/projects/bellatextiles_003.jpg"
     ]
+  },
+  {
+    id: "labruzzese",
+    title: "L'Abruzzese",
+    tags: "website",
+    description: null,
+    image_src: "labruz_01.jpg",
+    images: null
   }
 ];
 
